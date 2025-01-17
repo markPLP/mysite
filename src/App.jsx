ba8c310c4fd5f3228b6842d50ae70bdc4bc3a5d3
markPLP <mpclaspinas@gmail.com>
2025-01-17 13:34:23 +0800

    Create context and theme provider - dark - light - system mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import Header from './components/navbar/Header';
+import HeroBanner from './components/HeroBanner';
+import Navbar from './components/navbar/Navbar';
+import { ThemeProvider } from './components/theme-provider';
+import { SidebarProvider } from './context/SidebarContext';
+import NavbarMobile from './components/navbar/NavbarMobile';
+
 const App = () => {
   return (
-    <h1 className="text-6xl font-bold underline text-cyan-800">
-      Tailwind project
-    </h1>
+    <ThemeProvider>
+      <SidebarProvider>
+        <Header />
+        <Navbar />
+      </SidebarProvider>
+      <main>
+        <HeroBanner />
+      </main>
+      <NavbarMobile />
+    </ThemeProvider>
   );
 };
 export default App;
