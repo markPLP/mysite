efcc9fcf137193aacf0b335f9cf6ec7923561378
markPLP <mpclaspinas@gmail.com>
2025-01-21 08:42:33 +0800

    Memoize about components - added AboutInfoDescription.tsx

--- src/components/about/AboutInfo.tsx
+++ src/components/about/AboutInfo.tsx
@@ -1,4 +1,6 @@
 import { AboutData } from '@/utils/types';
+import AboutInfoDescription from './AboutInfoDescription';
+import React, { useMemo } from 'react';
 
 type AboutInfoProps = {
   currentItemId: string | null;
@@ -6,53 +8,24 @@ type AboutInfoProps = {
 };
 
 const AboutInfo = ({ currentItemId, data }: AboutInfoProps) => {
-  const currentItem = data.filter((item) => item.id === currentItemId);
-  console.log(currentItem);
-
-  type DescriptionItem = {
-    type: string;
-    items: string[];
-  };
-
-  const renderDescription = (
-    description: string | (string | DescriptionItem)[]
-  ) => {
-    if (Array.isArray(description)) {
-      return description.map((item, index) => {
-        if (typeof item === 'string') {
-          return (
-            <p key={index} className="mb-4">
-              {item}
-            </p>
-          );
-        }
-
-        if ((item as DescriptionItem).type === 'list') {
-          return (
-            <ul key={index} className="list-disc pl-5 mb-4">
-              {(item as DescriptionItem).items.map((listItem, listIndex) => (
-                <li key={listIndex}>{listItem}</li>
-              ))}
-            </ul>
-          );
-        }
-        return null;
-      });
-    }
-    return <p>{description}</p>;
-  };
+  const currentItem = useMemo(
+    () => data.filter((item) => item.id === currentItemId),
+    [currentItemId, data]
+  );
 
   return (
     <div>
       {currentItem.map((item) => {
         const { id, title, label, description } = item;
+        console.log(description, 'description');
+
         return (
           <div key={id}>
             <p className="first-letter:capitalize text-3xl mb-3">{label}</p>
             <h2 className="text-3xl font-bold text-primary mb-6 border-b-2 pb-3">
               {title}
             </h2>
-            {renderDescription(description)}
+            {AboutInfoDescription(description)}
           </div>
         );
       })}
@@ -60,4 +33,4 @@ const AboutInfo = ({ currentItemId, data }: AboutInfoProps) => {
   );
 };
 
-export default AboutInfo;
+export default React.memo(AboutInfo);
