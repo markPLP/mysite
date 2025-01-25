c65ad4edd3e92ee77c2ff943a33f7f0e0e1f81e1
markPLP <mpclaspinas@gmail.com>
2025-01-25 17:36:23 +0800

    Add suspense and lazy load components

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import Header from './components/navbar/Header';
-import HeroBanner from './components/hero/HeroBanner';
-import Navbar from './components/navbar/Navbar';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { Suspense, lazy } from 'react';
 import { ThemeProvider } from './components/theme-provider';
 import { SidebarProvider } from './context/SidebarContext';
-import NavbarMobile from './components/navbar/NavbarMobile';
-import About from './components/about/About';
-import Timeline from './components/experience/Timeline';
-import Projects from './components/projects/Projects';
-import { useFetchProjects } from './hooks/usefetchProjects';
+import Header from './components/navbar/Header';
+import Navbar from './components/navbar/Navbar';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-      // refetchOnWindowFocus: false,
-    },
-  },
-});
+// Lazy load components
+const NavbarMobile = lazy(() => import('./components/navbar/NavbarMobile'));
+const HeroBanner = lazy(() => import('./components/hero/HeroBanner'));
+const About = lazy(() => import('./components/about/About'));
+const Timeline = lazy(() => import('./components/experience/Timeline'));
+const Projects = lazy(() => import('./components/projects/Projects'));
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <SidebarProvider>
-          <Header />
-          <Navbar />
-        </SidebarProvider>
-        <main>
+    <ThemeProvider>
+      <SidebarProvider>
+        <Header />
+        <Navbar />
+      </SidebarProvider>
+      <main>
+        <Suspense fallback={<div>Loading Hero Banner...</div>}>
           <HeroBanner />
+        </Suspense>
+        <Suspense fallback={<div>Loading About Section...</div>}>
           <About />
+        </Suspense>
+        <Suspense fallback={<div>Loading Timeline...</div>}>
           <Timeline />
+        </Suspense>
+        <Suspense fallback={<div>Loading Projects...</div>}>
           <Projects />
-          {/* <Skills /> */}
-        </main>
+        </Suspense>
+      </main>
+      <Suspense fallback={<div>Loading Mobile Navbar...</div>}>
         <NavbarMobile />
-      </ThemeProvider>
-    </QueryClientProvider>
+      </Suspense>
+    </ThemeProvider>
   );
 };
+
 export default App;
